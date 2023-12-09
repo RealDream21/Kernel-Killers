75c97f7da542068b5c6ff6c18bf37ebcfbefdc87
Fabi <fabianflorete@yahoo.com>
2023-12-09 22:50:50 +0200

    python client configuration

---
+++ Fabi Login/Apitest.py
@@ -0,0 +1,14 @@
+from python_wireguard import Client, ServerConnection
+
+local_ip = "192.168.100.2/24"
+private = "S72aqA3SUpxEqZUM4OMw9g3cOXxRM7WQtz9UQVXd4CM="
+client = Client("wg-client", private, local_ip)
+
+srv_key = "DgLZYTtvqVkvt/TpOhfmZ8Pu+P5xrDj5Qlpnj+wEjU8="
+endpoint = "34.155.214.0"
+port = "51820"
+
+server_conn = ServerConnection(srv_key, endpoint, port)
+
+client.set_server(server_conn)
+client.connect()
