5f4e08d9f7720a70fc2f7ac3606f2896ad90b968
Dobrin Ionut <ionutdobrin2003@gmail.com>
2023-12-09 20:46:23 +0200

    Auto stash before merge of "main" and "origin/main"
    Some changes for a sample function of connect

--- 01-login-ionut/server.py
+++ 01-login-ionut/server.py
@@ -1,6 +1,8 @@
 """Python Flask WebApp Auth0 integration example
 """
-
+import subprocess
+import os
+import base64
 import json
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
@@ -70,6 +72,54 @@ def logout():
         )
     )
 
+
+
+@app.route("/connect", methods=["POST"])
+def connect_user(username):
+    # Replace this with the actual path to your WireGuard configuration directory
+    config_dir = "/path/to/wireguard/configs"
+
+    # Generate a new WireGuard private key for the user
+    private_key = subprocess.check_output(["wg", "genkey"]).decode("utf-8").strip()
+
+    # Derive the public key from the private key
+    public_key = subprocess.check_output(["wg", "pubkey"], input=private_key.encode("utf-8")).decode("utf-8").strip()
+
+    # Create a WireGuard configuration file for the user
+    config_content = f"""
+    [Interface]
+    PrivateKey = {private_key}
+    Address = 10.0.0.2/32
+    DNS = 8.8.8.8
+
+    [Peer]
+    PublicKey = {base64.b64encode(public_key.encode('utf-8')).decode('utf-8')}
+    AllowedIPs = 0.0.0.0/0
+    Endpoint = your_wireguard_server_ip:51820
+    """
+
+    user_config_path = os.path.join(config_dir, f"{username}.conf")
+
+    with open(user_config_path, "w") as config_file:
+        config_file.write(config_content)
+
+    # Add the user's public key to the WireGuard server configuration (replace with your actual server configuration)
+    subprocess.run(["wg", "set", "wg0", "peer", public_key, "allowed-ips", "10.0.0.2/32"])
+
+    return f"User connected to WireGuard. Configuration saved to {user_config_path}"
+
+
+
+
+
+
+
+@app.route("/disconnect", methods=["POST"])
+def disconnect_user():
+    # Implement logic to disconnect a user in WireGuard
+    # You may need to read user information from the session
+    # and use it to disassociate the user from the WireGuard configuration
+    return "User disconnected from WireGuard"
 
 
 
