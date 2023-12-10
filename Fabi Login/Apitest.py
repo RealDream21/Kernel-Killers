b1ef78f076f8f4b0ea93abb3093c8cfb801e16c0
Fabi <fabianflorete@yahoo.com>
2023-12-10 02:43:02 +0200

    Connections connect and disconnect

--- Fabi Login/Apitest.py
+++ Fabi Login/Apitest.py
@@ -1,4 +1,4 @@
-from python_wireguard import Client, ServerConnection
+"""from python_wireguard import Client, ServerConnection
 
 local_ip = "192.168.100.2/24"
 private = "S72aqA3SUpxEqZUM4OMw9g3cOXxRM7WQtz9UQVXd4CM="
@@ -12,3 +12,11 @@ server_conn = ServerConnection(srv_key, endpoint, port)
 
 client.set_server(server_conn)
 client.connect()
+"""
+
+from wireguard import Peer
+
+peer = Peer('my-client', '192.168.24.0/24', address='192.168.24.45')
+
+# Write out the peer config to the default location: /etc/wireguard/wg0.conf
+peer.config.write()
